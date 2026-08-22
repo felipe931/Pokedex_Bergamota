a269ff49a60b09263a3f261b4a7a01bb0620b961
felipe931 <felipe.manoel.lima.zatte@gmail.com>
2026-08-22 11:54:09 -0300

    sdsd

--- Server/controllers/PokemonController.js
+++ Server/controllers/PokemonController.js
@@ -21,13 +21,40 @@ async function createPokemons(req, res) {
     }
 }
 
-async function listPokemons(req, res) {
-    try {
-        const pokemons = await Pokemon.find();
-        res.status(200).json(pokemons);
-    } catch (error) {
-        res.status(500).json({ "Error ao listar pokémons": error.message });
-    }
+async function listPokemon(req, res) {
+ try {
+      const { name, type, minLevel, sortBy, order = "asc" } = req.query;
+   const filter = {};
+
+
+   if (name) {
+     filter.name = new RegExp(name, "i");
+   }
+
+
+   if (type) {
+     const typesArray = Array.isArray(type)
+       ? type
+       : type.split(",").map(t => t.trim());
+     filter.type = { $in: typesArray };
+   }
+
+
+   if (minLevel) {
+     filter.level = { $gte: Number(minLevel) };
+   }
+
+    const sortOption = {};
+if (sortBy) {
+  sortOption[sortBy] = order === "desc" ? -1 : 1;
+}
+
+ const pokemons = await Pokemon.find(filter).sort(sortOption);
+   res.status(200).json(pokemons);
+ } catch (error) {
+   console.error("Erro ao listar Pokemons:", error.message);
+   res.status(500).json({ error: "Erro ao listar Pokemons" });
+ }
 }
 
 async function updatePokemonById(req, res) {
